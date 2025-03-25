02a376dc03698724d0ef76917329a7b77a10890d
Riccardo <r.sannipoli@lineargruppo.it>
2025-03-25 13:26:27 +0100

    add test

--- api/tests/Api/OperationControllerTest.php
+++ api/tests/Api/OperationControllerTest.php
@@ -5,7 +5,6 @@ namespace App\Tests\Api;
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Enum\TipoOperazione;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
